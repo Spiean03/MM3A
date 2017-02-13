a92bed162a724172d2c577c25da122e402a77dee
Spiean03 <andreas.spielhofer@mail.mcgill.ca>
2017-02-13 18:22:45 -0500

    Add files via upload

---
+++ _defines.py
@@ -0,0 +1,13 @@
+# Serial and Baudrate for the Kleindiek MM3A Micromanipulator
+DEFAULT_SERIAL = 'COM20'
+DEFAULT_BAUDRATE = 115200
+
+
+# IP address, Port number for the Capres Four Point-Probe System
+
+DEFAULT_HOST = "192.168.1.80"
+DEFAULT_PORT = 23
+
+
+
+#DEFAULT_SERIAL = '/dev/ttyS0'
